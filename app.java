a4ef279b734acf842727853f17a6611090dbc1f5
puneethgowda1509 <puneethgowda1509@gmail.com>
2026-02-28 12:08:06 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,10 +1,3 @@
-public class app {
 
-    public static void main(String[] args) {
-
-        System.out.println("puneeth, World!");
-        System.out.println("Welcome to Java Programming!");
-
-    }
-
-}
+name = input("What is your name? ")
+print("Hello, " + puneeth + "! Welcome to Python.")
